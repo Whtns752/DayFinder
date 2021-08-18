8d4e9b38677b9ba25db4a83b94063ae9ba2b5678
WhiteNoise <whtns752@gmail.com>
2021-08-18 13:50:30 +0300

    Added OptionalCheck

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findButton(_ sender: UIButton) {
+        
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else {return}
+        
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else {return}
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
+        
+        resultLabel.text = weekday
+        
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+   }
 
 
-}
+
 
